f6de4a1f3e0c62defe23d932b7f1b4d8a6878baf
Jonathan Engelsma <jengelsma@gmail.com>
2020-08-18 13:22:50 -0400

    Initial Commit

---
+++ TraxyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TraxyApp
+//
+//  Created by Jonathan Engelsma on 8/18/20.
+//  Copyright © 2020 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TraxyAppTests/TraxyAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TraxyAppTests.swift
+//  TraxyAppTests
+//
+//  Created by Jonathan Engelsma on 8/18/20.
+//  Copyright © 2020 Jonathan Engelsma. All rights reserved.
+//
+
+import XCTest
+@testable import TraxyApp
+
+class TraxyAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
