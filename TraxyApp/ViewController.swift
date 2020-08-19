6c9cc6f61bed91cc6cdbd5562ae82b70546acb08
Jonathan Engelsma <jengelsma@gmail.com>
2020-08-19 18:18:29 -0400

    Ch02 updates.

--- TraxyApp/ViewController.swift
+++ TraxyApp/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // dismiss keyboard when tapping outside of text fields
+        let detectTouch = UITapGestureRecognizer(target: self, action:
+        #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+                                          
+        // make this controller the delegate of the text fields.
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+    }
+    
+    @objc func dismissKeyboard() {
+      self.view.endEditing(true)
     }
 
+    func validateFields() -> Bool {
+      var pwOk = false
+      if let pw = self.passwordField.text {
+          if pw.lowercased().range(of: "traxy") != nil {
+              pwOk = true
+          }
+      }
+      if !pwOk {
+        print(NSLocalizedString("Invalid password", comment: ""))
+      }
 
+                              
+      var emailOk = false
+      if let email = self.emailField.text {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", regex)
+        emailOk = emailPredicate.evaluate(with: email)
+      }
+      if !emailOk {
+        print(NSLocalizedString("Invalid email address", comment: ""))
+      }
+      return emailOk && pwOk
+    }
+
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        if self.validateFields() {
+            print(NSLocalizedString("Congratulations!  You entered correct values.", comment: ""))
+        }
+    }
+    
 }
 
+extension ViewController : UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField == self.emailField {
+      self.passwordField.becomeFirstResponder()
+    } else {
+      if self.validateFields() {
+        print(NSLocalizedString("Congratulations!  You entered correct values.", comment: ""))
+      }
+    }
+    return true
+  }
+}
