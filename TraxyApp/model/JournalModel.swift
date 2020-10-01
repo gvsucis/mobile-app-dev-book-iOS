6b3ef766d11cdf801ce7b1dd35e460b8d74d2f98
Jonathan Engelsma <jengelsma@gmail.com>
2020-10-01 09:51:28 -0400

    Restructured source files.

---
+++ TraxyApp/model/JournalModel.swift
@@ -0,0 +1,42 @@
+//
+//  JournalModel.swift
+//  TraxyApp
+//
+//  Created by Jonathan Engelsma on 9/30/20.
+//  Copyright © 2020 Jonathan Engelsma. All rights reserved.
+//
+
+import Foundation
+class JournalModel {
+    fileprivate var items : [Journal] = [Journal]()
+    
+    init() {
+        createJournals()
+    }
+    
+    func getJournals() -> [Journal]
+    {
+        return self.items
+    }
+    
+    fileprivate func createJournals()
+    {
+        items.append(Journal(name: "Out West", location: "Estes Park, Co",
+                             startDate: Date.distantFuture, endDate: Date.init(timeInterval: 100000,
+                                                                               since: Date.distantFuture), lat: 40.3772059, lng: -105.5216651,
+                             placeId: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(Journal(name: "Out West", location: "Estes Park, Co",
+                             startDate: Date.distantFuture, endDate: Date.init(timeInterval: 100000,
+                                                                               since: Date.distantFuture), lat: 40.3772059, lng: -105.5216651,
+                             placeId: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(Journal(name: "Down South", location: "Charlotte, NC",
+                             startDate: Date.distantFuture, endDate: Date.init(timeInterval: 100000,
+                                                                               since: Date.distantFuture), lat: 40.3772059, lng: -105.5216651,
+                             placeId: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+        items.append(Journal(name: "Out East", location: "New York, NY",
+                             startDate: Date.distantFuture, endDate: Date.init(timeInterval: 100000,
+                                                                               since: Date.distantFuture), lat: 40.3772059, lng: -105.5216651,
+                             placeId: "ChIJAxoZu9ZlaYcRDKzKqbeYlts"))
+    }
+}
+
